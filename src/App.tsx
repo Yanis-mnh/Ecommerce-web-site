989d55445de3dc71bc9071fa67a94b5efdaf5a3f
Yanis-mnh <monnah.yans@gmail.com>
2024-08-18 16:43:01 +0100

    add: chakraUI to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return (
-    <ChakraProvider>
-      <div>
-        <p>test</p>
-      </div>
-    </ChakraProvider>
-  );
+  return <></>;
 }
 
 export default App;
